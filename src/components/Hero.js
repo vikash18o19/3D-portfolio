e63d5abf8fbfe7a457c4c305442cc9b3a0f3b9c1
Vikash Kumar <vikash0cv@gmail.com>
2023-06-02 03:32:08 +0530

    removed Navbar

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,6 @@
-import React from 'react'
-import styled from 'styled-components'
-import Navbar from './Navbar'
-
-
+import React from "react";
+import styled from "styled-components";
+import Navbar from "./Navbar";
 
 const Section = styled.div`
   height: 100vh;
@@ -11,106 +9,98 @@ const Section = styled.div`
   align-items: center;
   justify-content: space-between;
   flex-direction: column;
-`
+`;
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
-  padding-left:20vw;
-  padding-right: 5vw;
   /* scroll-snap-align: center; */
   display: flex;
   justify-content: center;
-`
+`;
 const Left = styled.div`
-    padding: 5vh 10vw;   
-    display: flex;
-    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-    flex-direction: column;
-    /* align-items: center; */
-    justify-content: center;
-    gap: 5svh;
-
-`
+  /* padding-right: 5svw; */
+  width: 40svw;
+  display: flex;
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+  flex-direction: column;
+  /* align-items: center; */
+  justify-content: center;
+  gap: 5svh;
+  @media only screen and (max-width: 768px) {
+    /* padding-left: initial; */
+  }
+`;
 const Title = styled.h1`
-    font-size: 5rem;
-    color: white;
-`
+  font-size: 5rem;
+  color: white;
+`;
 
 // const Line = styled.img`
 //     width:20px;
 // `
 const WhatWeDo = styled.div`
-    display: flex;
-    align-items: center;
-    gap: 10px;
-    color: white;
-`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  color: white;
+`;
 const Subtitle = styled.h2`
-    font-size: 35px;
-    color: white;
-`
+  font-size: 35px;
+  color: white;
+`;
 const Desc = styled.p`
-    color: white;
-    font-size: 25px;
-`
+  color: white;
+  font-size: 25px;
+`;
 const Button = styled.button`
-    cursor: pointer;
-    border: none;
-    border-radius: 5px;
-    width: 150px;
-    height: 30px;
-`
-
+  cursor: pointer;
+  border: none;
+  border-radius: 5px;
+  width: 150px;
+  height: 30px;
+`;
 
 const Right = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-right: 0;
-    @media only screen and (max-width: 768px) {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 0;
+  @media only screen and (max-width: 768px) {
     display: none;
   }
-`
+`;
 const Img = styled.img`
-    height: 50svh;
-    width: 60svw;
-    object-fit: contain;
-    animation: animate 3s infinite ease alternate;
+  height: 50svh;
+  width: 20svw;
+  object-fit: contain;
+  animation: animate 3s infinite ease alternate;
 
-    @keyframes animate {
-        100%{
-            transform: translateY(20px);
-        }
+  @keyframes animate {
+    100% {
+      transform: translateY(20px);
     }
-`
+  }
+`;
 
+const Hero = (props) => {
+  return (
+    <Section>
+      {/* <Navbar /> */}
+      <Container>
+        <Left>
+          <Title>Welcome...</Title>
+          <WhatWeDo>
+            {/* <Line src="./images/white_line.png" /> */}
+            <Subtitle> -- What I do..</Subtitle>
+          </WhatWeDo>
+          <Desc>
+            I enjoy creating apps having great visuals and involving AI.
+          </Desc>
+          <Button>Learn More</Button>
+        </Left>
+      </Container>
+    </Section>
+  );
+};
 
-
-
-
-
-const Hero = () => {
-
-
-    return (
-        <Section>
-            <Navbar />
-            <Container>
-                <Left>
-                    <Title>Welcome...</Title>
-                    <WhatWeDo>
-                        {/* <Line src="./images/white_line.png" /> */}
-                        <Subtitle> -- What I do..</Subtitle>
-                    </WhatWeDo>
-                    <Desc>I enjoy creating apps having great visuals and involving AI.</Desc>
-                    <Button>Learn More</Button>
-                </Left>
-                <Right>
-                    <Img src="./images/astronaut.png" />
-                </Right>
-            </Container>
-        </Section >
-    )
-}
-
-export default Hero
+export default Hero;
